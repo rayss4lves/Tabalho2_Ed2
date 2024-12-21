05d556ae4481408c5d900c22bd21f5a898b7d958
rayss4lves <rayssasantos.com890@gmail.com>
2024-12-21 13:06:48 -0300

    correção case 4

--- questao_1/main.c
+++ questao_1/main.c
@@ -103,7 +103,8 @@ int main()
         case 4:
             printf("\n--------------------------------------------------------------- \n");
             printf("Insira a palavra em portugues que deseja remover: ");
-            scanf("%s", palavra);
+            setbuf(stdin, NULL);
+            scanf("%[^\n]", palavra);
             removido = remover23(&pai, &raiz, palavra);
             if (removido)
                 printf("A palavra %s foi removida com sucesso!\n\n", palavra);
